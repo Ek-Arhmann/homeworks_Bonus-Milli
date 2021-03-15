8be2a083e097f1445543b87f96cbb6636dbb6cd4
Yurii Arkhipenko <ek.arhmann@gmail.com>
2021-03-15 11:38:58 +0300

    add program

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args ){
+        int ticketPrice = 25400;
+                int bonus = 20;
+                int amount = (ticketPrice / bonus);
+                System.out.println(amount);
+    }
+}
